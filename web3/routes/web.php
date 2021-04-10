b2025ac337023f434a7c7394a51ec345b4daa18e
yang yizhou <yangyizhou99@gmail.com>
2021-04-10 22:29:53 +0200

    Route

--- web3/routes/web.php
+++ web3/routes/web.php
@@ -19,53 +19,53 @@ Route::get('/news/{id}',function($id){
 // public of news
 Route::get('/news/create',function(){
     dd('create_news');
-});
+})->name('news.create');
 
 //submit news
 Route::post('/news',function(){
     dd('save_news');
-});
+})->name('news.store');
 
 Route::get('/news/{id}/edit',function($id){
     dd('edit news :'.$id);
-});
+})->name('news.edit');
 
 //to database
 Route::put('/news/{id}',function($id){
     dd('edit saved ,news '.$id);
-});
+})->name('news.update');
 
 //delete
 
 Route::delete('/news/{id}',function($id){
     dd('delete news:'.$id);
-});
+})->name('news.destroy');
 
 // hide news or show it
 Route::patch('/news/{id}',function($id){
     dd('state want to change'.$id);
-});
+})->name('new.status');
 
 Route::get('/user',function(){
     dd('change profile');
-});
+})->name('user.info');
 
 // update information
 Route::put('/user',function(){
-    dd('change');
-});
+    dd('change comfirm');
+})->name('user.update');
 
 Route::get('/user/avatar',function(){
     dd('change photo');
-});
+})->name('user.avatar');
 
 Route::patch('/user/avatar',function(){
     dd('change photo comfirm');
-});
+})->name('user.avatar.update');
 
 //all news
 Route::get('/user/news',function(){
     dd('all my news');
-});
+})->name('user.news');
 
 //login registration
