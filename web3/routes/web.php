ccfa85108ef548f4bd25750830f9bbd878c68d05
yang yizhou <yangyizhou99@gmail.com>
2021-04-10 22:21:16 +0200

    route

--- web3/routes/web.php
+++ web3/routes/web.php
@@ -2,17 +2,70 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
 Route::get('/', function () {
     return view('welcome');
 });
+
+//home
+Route::get('/',function(){
+    dd('home');
+});
+
+//detail
+Route::get('/news/{id}',function($id){
+    dd('detail of :'.$id);
+})->name('news.show')->where('id','[0-9]');
+
+// public of news
+Route::get('/news/create',function(){
+    dd('create_news');
+});
+
+//submit news
+Route::post('/news',function(){
+    dd('save_news');
+});
+
+Route::get('/news/{id}/edit',function($id){
+    dd('edit news :'.$id);
+});
+
+//to database
+Route::put('/news/{id}',function($id){
+    dd('edit saved ,news '.$id);
+});
+
+//delete
+
+Route::delete('/news/{id}',function($id){
+    dd('delete news:'.$id);
+});
+
+// hide news or show it
+Route::patch('/news/{id}',function($id){
+    dd('state want to change'.$id);
+});
+
+Route::get('/user',function(){
+    dd('change profile');
+});
+
+// update information
+Route::put('/user',function(){
+    dd('change');
+});
+
+Route::get('/user/avatar',function(){
+    dd('change photo');
+});
+
+Route::patch('/user/avatar',function(){
+    dd('change photo comfirm');
+});
+
+//all news
+Route::get('/user/news',function(){
+    dd('all my news');
+});
+
+//login registration
